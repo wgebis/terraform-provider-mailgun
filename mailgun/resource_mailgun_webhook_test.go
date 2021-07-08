99a1f9f025f9c139f691d1833b8c9982d4be6ac3
yagince <straitwalk@gmail.com>
2021-07-08 12:14:13 +0900

    Support Webhook

---
+++ mailgun/resource_mailgun_webhook_test.go
@@ -0,0 +1,69 @@
+package mailgun
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/go-uuid"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccMailgunWebhook_Basic(t *testing.T) {
+
+	uuid, _ := uuid.GenerateUUID()
+	domain := fmt.Sprintf("terraformcred.%s.com", uuid)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: newProvider(),
+		CheckDestroy:      testAccCheckMailgunWebhookDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckMailgunWebhookConfig(domain),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"mailgun_webhook.foobar", "domain", domain),
+					resource.TestCheckResourceAttr(
+						"mailgun_webhook.foobar", "region", "us"),
+					resource.TestCheckResourceAttr(
+						"mailgun_webhook.foobar", "kind", "delivered"),
+					resource.TestCheckResourceAttr(
+						"mailgun_webhook.foobar", "urls.0", "https://hoge.com"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckMailgunWebhookDestroy(s *terraform.State) error {
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "mailgun_webhook" {
+			continue
+		}
+
+		client, _ := testAccProvider.Meta().(*Config).GetClientForDomain(rs.Primary.Attributes["region"], rs.Primary.Attributes["domain"])
+
+		kind := rs.Primary.Attributes["kind"]
+		webhooks, err := client.GetWebhook(context.Background(), kind)
+
+		if err == nil {
+			return fmt.Errorf("Webhook still exists: %#v", webhooks)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckMailgunWebhookConfig(domain string) string {
+	return `
+resource "mailgun_webhook" "foobar" {
+  domain = "` + domain + `"
+  region = "us"
+  kind = "delivered"
+  urls = ["https://hoge.com"]
+}`
+}
